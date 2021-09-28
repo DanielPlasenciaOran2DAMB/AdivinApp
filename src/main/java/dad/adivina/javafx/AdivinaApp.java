90ece766e355c5ea50bc87226453008b681077bb
DanielPlasenciaOran2DAMB <2bacdanielplasencia@gmail.com>
2021-09-28 12:38:19 +0100

    Acabado de la aplicacion

--- src/main/java/dad/adivina/javafx/AdivinaApp.java
+++ src/main/java/dad/adivina/javafx/AdivinaApp.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -12,10 +14,22 @@ import javafx.stage.Stage;
 
 public class AdivinaApp extends Application {
 
+	public static int Aleatorio() {
+		int numeroAdivina = (int) ((Math.random() * 100) + 1);
+		return numeroAdivina;
+	}
+
 	private Label numeroLabel;
 	private TextField numeroText;
 	private Button comprobarButton;
 	private VBox rootPanel;
+	private Integer numeroAleatorio = Aleatorio();
+	private Integer intentos = 1;
+
+	public void reincicioApp() {
+		numeroAleatorio = Aleatorio();
+		intentos = 0;
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -46,14 +60,44 @@ public class AdivinaApp extends Application {
 
 	private void onComprobarButtonAction(ActionEvent e) {
 
-		int numeroAleatorio = (int) ((Math.random() * 100) + 1);
-		int i;
-		
-		for (i = 0; i <= 0; i++) {
-			if (numeroAleatorio == numeroText.getText()) {
-				
+		try {
+			if (numeroAleatorio == Integer.parseInt(numeroText.getText())) {
+				Alert ganadorAlert = new Alert(AlertType.INFORMATION);
+				ganadorAlert.setTitle("AdivinaApp");
+				ganadorAlert.setHeaderText("¡Has ganado!");
+				ganadorAlert.setContentText(
+						"Sólo has necesitado " + intentos + " intentos. \n\n Vuelve a jugar y hazlo mejor");
+
+				ganadorAlert.showAndWait();
+
+				reincicioApp();
+			} else if (numeroAleatorio > Integer.parseInt(numeroText.getText())) {
+				Alert pistaMayorAlert = new Alert(AlertType.WARNING);
+				pistaMayorAlert.setTitle("AdivinaApp");
+				pistaMayorAlert.setHeaderText("¡Has fallado!");
+				pistaMayorAlert.setContentText("El número a adivinar es mayor que "
+						+ Integer.parseInt(numeroText.getText()) + ". \n\n Vuelve a intentarlo.");
+
+				pistaMayorAlert.showAndWait();
+			} else if (numeroAleatorio < Integer.parseInt(numeroText.getText())) {
+				Alert pistaMenorAlert = new Alert(AlertType.WARNING);
+				pistaMenorAlert.setTitle("AdivinaApp");
+				pistaMenorAlert.setHeaderText("¡Has fallado!");
+				pistaMenorAlert.setContentText("El número a adivinar es menor que "
+						+ Integer.parseInt(numeroText.getText()) + ". \n\n Vuelve a intentarlo.");
+
+				pistaMenorAlert.showAndWait();
 			}
+		} catch (NumberFormatException error) {
+			Alert errorAlert = new Alert(AlertType.ERROR);
+			errorAlert.setTitle("AdivinaApp");
+			errorAlert.setHeaderText("Error");
+			errorAlert.setContentText("El número introducido no es válido.");
+
+			errorAlert.showAndWait();
 		}
+
+		intentos++;
 	}
 
 	public static void main(String[] args) {
